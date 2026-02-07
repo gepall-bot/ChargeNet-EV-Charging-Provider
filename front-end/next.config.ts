e235556b69e19d4382429dfc11c4a13a7d042de9
ntua-el22039 <el22039@mail.ntua.gr>
2026-02-07 21:49:29 +0200

    Fixing some stuff

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -5,6 +5,20 @@ const nextConfig = {
     'http://127.0.0.1:3000',
     'http://192.168.1.8:3000', // phone accessing your PC
   ],
+  turbopack: {
+    root: __dirname,
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: '/api/v1',
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: 'http://127.0.0.1:9876/api/v1/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
